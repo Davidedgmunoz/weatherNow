95490754b8f905130ebed3f93bd1c223901ffab2
DedgMz <David.edg.munoz@gmail.com>
2024-11-26 15:33:05 -0300

    Initial Commit

---
+++ WeatherNowTests/WeatherNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowTests.swift
+//  WeatherNowTests
+//
+//  Created by David Muñoz on 26/11/2024.
+//
+
+import Testing
+@testable import WeatherNow
+
+struct WeatherNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
